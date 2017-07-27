fc2fa9566e7fb349fda86b758a3e0dbff3810a72
Harry Pierson <harrypierson@outlook.com>
2017-07-27 07:25:19 -0700

    Minor lifetime control changes

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -60,6 +60,7 @@ struct SampleCallback : public RuntimeClass<
     {
     }
 
+private:
     ~SampleCallback()
     {
         _subscriber.on_completed();
@@ -67,17 +68,24 @@ struct SampleCallback : public RuntimeClass<
 
     HRESULT STDMETHODCALLTYPE OnComplete(_In_ HRESULT result, uint64_t requestId) override
     {
+        printf("OnComplete %lld %ld\n", requestId, result);
         _subscriber.on_next(std::make_tuple(requestId, result));
         return S_OK;
     }
 
     HRESULT STDMETHODCALLTYPE OnError(_In_ HRESULT result, uint64_t requestId) override
     {
+        printf("OnError %lld %ld\n", requestId, result);
         _subscriber.on_next(std::make_tuple(requestId, result));
         return S_OK;
     }
 
-private:
+    // I tried both the current approach (passing the subscriber in via the ctor) as well
+    // as having the subject inside the callback. Since the callback is an internal implementation
+    // detail of the adapter, I think it's better off this way and letting the adapter own the 
+    // subject. Will have to see what happens when the subject is torn down before the subscriber
+    // but I'll need to build out the adapter to see. I think the subscription sticks around as 
+    // long as someone has a reference to it somewhere.
     rxcpp::subscriber<std::tuple<uint64_t, HRESULT>> _subscriber;
 };
 
@@ -102,17 +110,19 @@ int main()
     HRESULT hr = GetSampleClient(client.GetAddressOf());
     if (FAILED(hr)) { throw std::exception("GetSampleClient", hr); }
 
-    rxcpp::subjects::subject<std::tuple<uint64_t, HRESULT>> subject;
-
-    ComPtr<ISampleCallback> callback = Make<SampleCallback>(subject.get_subscriber());
-    hr = client->Listen(callback.Get());
-    if (FAILED(hr)) { throw std::exception("client->Listen", hr); }
-    callback.Reset();
-
-    subject.get_observable().subscribe([](std::tuple<uint64_t, HRESULT> t)
     {
-        printf("OnNext %lld %ld\n", std::get<0>(t), std::get<1>(t));
-    });
+        rxcpp::subjects::subject<std::tuple<uint64_t, HRESULT>> subject;
+
+        ComPtr<ISampleCallback> callback = Make<SampleCallback>(subject.get_subscriber());
+        hr = client->Listen(callback.Get());
+        if (FAILED(hr)) { throw std::exception("client->Listen", hr); }
+        callback.Reset();
+
+        subject.get_observable().subscribe([](std::tuple<uint64_t, HRESULT> t)
+        {
+            printf("OnNext %lld %ld\n", std::get<0>(t), std::get<1>(t));
+        });
+    }
 
     make_request(client);
     make_request(client);
